e65ba96b521bbb91015566a47eb3bbab6a0d40f9
Burak Emre Kabakcı <emrekabakci@gmail.com>
2025-08-31 01:12:05 +0100

    chore: remove obsolete TypeScript declaration and JavaScript files
    - Deleted `index.d.ts`, `index.js`, and their corresponding source maps from the shared package as they are no longer needed.
    - This cleanup helps streamline the codebase and eliminate unused artifacts.

--- packages/shared/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type { ClaudeExecutionOptions, SessionContext, ConversationMessage, } from "./types";
-export { SessionUtils } from "./session-utils";
-//# sourceMappingURL=index.d.ts.map
--- packages/shared/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SessionUtils = void 0;
-// Export utilities
-var session_utils_1 = require("./session-utils");
-Object.defineProperty(exports, "SessionUtils", { enumerable: true, get: function () { return session_utils_1.SessionUtils; } });
-//# sourceMappingURL=index.js.map
